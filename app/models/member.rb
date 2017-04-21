2fc9342b99f9eca81222f76c90bea538b6c43721
Anders Howerton <anders.e.howerton@gmail.com>
2017-04-21 09:38:35 -0700

    don't save transaction if it puts member below overdrawn limit

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,12 +1,17 @@
 class Member < ApplicationRecord
   has_many :transactions
-  validates :name, presence: { strict: true }
+  validates :name, presence: true
   validates :email, uniqueness: true, presence: true
   validates :balance, numericality: { greater_than_or_equal_to: -150.00 }	
 
 
   def compute_balance(transaction_amount)
-    self.balance += transaction_amount
-    self.save!
+  	if (self.balance + transaction_amount) < -150.00
+      return false
+    else
+      self.balance += transaction_amount
+      self.save!
+    end
   end
+
 end
